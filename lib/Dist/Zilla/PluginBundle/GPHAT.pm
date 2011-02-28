1e59a55f595806e5a67261ed4fa501760f161b1b
Cory Watson <cwatson@magazines.com>
2011-02-28 08:14:58 -0600

    Closer...

--- lib/Dist/Zilla/PluginBundle/GPHAT.pm
+++ lib/Dist/Zilla/PluginBundle/GPHAT.pm
@@ -1,11 +1,13 @@
 package Dist::Zilla::PluginBundle::GPHAT;
 use Moose;
 
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+
 # ABSTRACT: Dist::Zilla plugins for gphat
 
 =head1 DESCRIPTION
 
-This is my preferred Dist::Zilla setup.  It's equivalent to:
+This is the plugin bundle that GPHAT uses.  It's equivalent to:
 
     [@Basic]
 
@@ -18,10 +20,6 @@ This is my preferred Dist::Zilla setup.  It's equivalent to:
     [PkgVersion]
 
     [AutoPrereqs]
-
-    [Prereqs]
-    JSON::Any       = 0
-    JSON::XS        = 0
 
     [NextRelease]
     format          = %v %{MMM d yyyy}d
@@ -50,5 +48,55 @@ This is my preferred Dist::Zilla setup.  It's equivalent to:
     [PodWeaver]
 
 =cut
+
+sub configure {
+    my ($self) = @_;
+
+    $self->add_bundle('@Basic');
+
+    $self->add_plugins([
+        'Git::NextVersion' => {
+            first_version  => '0.01',
+            version_regexp => '^(\d+\.\d+)$'
+        }
+    ]);
+
+    $self->add_plugins(qw(
+        ReadmeMarkdownFromPod
+        PkgVersion
+        AutoPrereqs
+    ));
+
+    $self->add_plugins([
+        'NextRelease' => {
+            format => '%v %{MMM d yyyy}d'
+        }
+    ]);
+
+    $self->add_plugins(qw(
+        SynopsisTests
+        PodSyntaxTests
+        MetaJson
+    ));
+
+    $self->add_plugins([
+        'CopyFilesFromBuild' => {
+            format => 'README.mkdn'
+        }
+    ]);
+
+    $self->add_plugins(qw(Git::Commit));
+
+    $self->add_plugins([
+        'Git::Tag' => {
+            tag_format => '%v'
+        }
+    ]);
+
+    $self->add_plugins(qw(PodWeaver));
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
 
 1;
